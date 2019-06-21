b41cea7df353827240e2c12a54aee8ffa41d3897
taoshowa <taoshowad@gmail.com>
2019-06-21 11:16:38 +0800

    fix code

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
   rules: {
+    "vue/singleline-html-element-content-newline": "off",
     "vue/max-attributes-per-line": [2, {
       "singleline": 10,
       "multiline": {
